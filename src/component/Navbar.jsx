1d523375bb98f923841ab955a3d8ffdf108a0e41
Dikoi <dicky.susanto@binus.ac.id>
2025-02-08 17:33:07 +0700

    fix: fixing navbar responsive

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const navLinks = [
-  { name: "Home", href: "#home" },
-  { name: "About Me", href: "#about" },
-  { name: "Technologies", href: "#technologies" },
-  { name: "Experiences", href: "#experiences" },
-  { name: "Projects", href: "#projects" },
-  { name: "Contacts", href: "#contacts" },
+  { name: 'Home', href: '#home' },
+  { name: 'About Me', href: '#about' },
+  { name: 'Technologies', href: '#technologies' },
+  { name: 'Experiences', href: '#experiences' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Contacts', href: '#contacts' },
 ];
 
 const Navbar = () => {
@@ -27,61 +27,38 @@ const Navbar = () => {
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
-    <nav
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-        scrolled
-          ? "bg-gray-800 bg-opacity-40 backdrop-blur-md"
-          : "bg-transparent"
-      } text-white py-6 px-4 sm:px-8 lg:px-16`}
-    >
+    <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${scrolled ? 'bg-gray-800 bg-opacity-40 backdrop-blur-md' : 'bg-transparent'} text-white py-6 px-4 sm:px-8 lg:px-16`}>
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex flex-shrink-0 items-center">
-          <a
-            href="#"
-            className="text-4xl font-bold relative group transition-all duration-300"
-          >
-            <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
-              Dikoi
-            </span>
+          <a href="#" className="text-4xl font-bold relative group transition-all duration-300">
+            <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">Dikoi</span>
             <span className="absolute -bottom-1 left-0 w-0 h-1 bg-pink-400 transition-all duration-300 group-hover:w-full"></span>
           </a>
         </div>
         <div className="hidden md:flex space-x-8">
           {navLinks.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="hover:text-pink-500 transition-colors duration-300"
-            >
+            <a key={link.name} href={link.href} className="hover:text-pink-500 transition-colors duration-300">
               {link.name}
             </a>
           ))}
         </div>
         <div className="md:hidden">
-          <button
-            onClick={toggleMobileMenu}
-            className="text-2xl focus:outline-none"
-          >
+          <button onClick={toggleMobileMenu} className="text-2xl focus:outline-none">
             {isOpen ? <FaTimes /> : <FaBars />}
           </button>
         </div>
       </div>
 
       {isOpen && (
-        <div className="md:hidden mt-4">
+        <div className="md:hidden mt-4 bg-gray-950  p-6 rounded-lg shadow-lg">
           <div className="flex flex-col space-y-4">
             {navLinks.map((link) => (
-              <a
-                key={link.name}
-                href={link.href}
-                onClick={() => setIsOpen(false)}
-                className="block hover:text-pink-500 transition-colors duration-300"
-              >
+              <a key={link.name} href={link.href} onClick={() => setIsOpen(false)} className="block hover:text-pink-500 transition-colors duration-300">
                 {link.name}
               </a>
             ))}
